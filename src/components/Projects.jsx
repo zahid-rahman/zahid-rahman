346a4d956c4d9746c01b46ae13c79cb6df3097ba
corbin009 <rahmanzahid298@gmail.com>
2021-02-10 00:12:19 +0600

    WIP:added three more components

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>Projects</h1>
+            <p>this is projects component</p>
+        </div>
+    )
+};
+
+export default Projects;
---
+++ src/components/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>this is resume component</p>
+        </div>
+    )
+};
+
+export default Resume;
