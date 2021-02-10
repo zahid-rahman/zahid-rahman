fbfbf6b8eaba267dda283f69ab9c17b87f2d72a5
corbin009 <rahmanzahid298@gmail.com>
2021-02-10 19:02:57 +0600

    FIX:added project details functionality

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import projectData from '../json/projectDetails'
+import ProjectCard from './ProjectCard';
 
 const Projects = () => {
+    const [projects,setProjects] = useState(projectData)
+
+    const filterCategoryAction = (category) => {
+        const filteredProjectData = projectData.filter(project => project.category.includes(category))
+        setProjects(filteredProjectData)
+    }
+
+    const projectDetailsCard = projects.map((projectDetails,index) => {
+        return <ProjectCard key={index} value={projectDetails}></ProjectCard>
+    })
+    
     return (
-        <div>
-            <h1>Projects</h1>
-            <p>this is projects component</p>
+        <div className="container projectContainer">
+            <div className="projectNavbar">
+                <div onClick={() => setProjects(projectData)} >All</div>
+                <div onClick={() => filterCategoryAction('nodejs')}>Node js</div>
+                <div onClick={() => filterCategoryAction('reactjs')}>React</div>
+                <div onClick={() => filterCategoryAction('mern_stack')}>Mern stack</div>
+                <div onClick={() => filterCategoryAction('vanilla')}>Vanilla</div>
+                <div onClick={() => filterCategoryAction('laravel')}>Laravel</div>
+            </div>
+            <div className="row">
+                {projectDetailsCard}
+            </div>
         </div>
     )
 };
